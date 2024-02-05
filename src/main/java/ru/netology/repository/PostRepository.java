8dfab3bf870e931d94ca82f59a5848cff4af89a0
CaramelizedPrincess <133921403+CaramelizedPrincess@users.noreply.github.com>
2024-02-05 17:13:04 +0300

    Add files via upload

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,42 @@
+package ru.netology.repository;
+
+
+import ru.netology.model.Post;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+
+public class PostRepository {
+  private final AtomicLong userId;
+  private final Map<Long, Post> listofPosts;
+
+  public PostRepository() {
+    userId = new AtomicLong(0);
+    listofPosts = new ConcurrentHashMap<>();
+  }
+
+  public List<Post> all() {
+    return new ArrayList<>(listofPosts.values());
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(listofPosts.get(id));
+  }
+
+  public Post save(Post post) {
+    long specificID = post.getId();
+    if (specificID > 0 && listofPosts.containsKey(specificID)) {
+      listofPosts.replace(specificID, post);
+    } else {
+      long newIdPost = specificID == 0 ? userId.incrementAndGet() : specificID;
+      post.setId(newIdPost);
+      listofPosts.put(newIdPost, post);
+    }
+    return post;
+  }
+
+  public void removeById(long id) {
+    listofPosts.remove(id);
+  }
+}
